b06b79ed836afbb80b9c2d0fdefced94dcd89bbd
Thiago <thiagosalaberry99@gmail.com>
2024-01-18 14:51:35 -0300

    cors endpoint

---
+++ pages/api/cors/index.ts
@@ -0,0 +1,22 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import Cors from "cors";
+const cors = Cors({
+    methods: ["GET", "POST", "PATCH"]
+});
+function runMiddleware(req:NextApiRequest, res:NextApiResponse, fn:Function) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result:any) => {
+            if (result instanceof Error) {
+                return reject(result);
+            };
+            return resolve(result);
+        })
+    })
+}
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    await runMiddleware(req, res, cors);
+    res.json({
+        message: "Este es un endpoint protegido por CORS",
+        method: req.method
+    });
+}
