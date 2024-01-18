b2aad6ef2677e5b048ce3885976d240420723347
Thiago <thiagosalaberry99@gmail.com>
2024-01-18 16:59:36 -0300

    correcion de cors (auth y token)

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -1,23 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkEmailAndCode } from "../../../controllers/authControllers";
 import methods from "micro-method-router";
-import corsMiddleware from "../../../lib/corsMiddleware";
+import { runMiddleware } from "../../../lib/corsMiddleware";
+import Cors from "cors";
+const cors = Cors({
+    methods: ["GET", "POST", "PATCH"]
+  });
 //Este endpoint se encarga de recibir un email y un code desde el body
 //De ser correctos, devuelve un token e invalida el código
-/*export default*/ async function token(req:NextApiRequest, res:NextApiResponse) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    await runMiddleware(req, res, cors);
     const { email, code } = req.body;
     if (!email || !code) res.status(400).json({message: "Debes ingresar un email y un código para poder ingresar."});
     const token = await checkEmailAndCode(email, code);
     if (!token) res.status(403).json({message: "Los datos ingresados son incorrectos."});
     res.status(200).json(token);
 };
-
-const handler = methods({
-    post: token
-});
-
-const corsHandler = async (req:NextApiRequest, res:NextApiResponse) => {
-    return await corsMiddleware(req, res, handler)
-};
-
-export default corsHandler;
