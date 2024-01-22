c2a48932c0cd53e0d0f0d45893e6212bd033637f
Thiago <thiagosalaberry99@gmail.com>
2024-01-21 23:35:55 -0300

    cors /me 2

--- lib/authMiddleware.ts
+++ lib/authMiddleware.ts
@@ -1,8 +1,10 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import { verifyToken } from "./jwt";
 import parseToken from "parse-bearer-token";
+import {runMiddleware} from "./corsMiddleware";
 export function authMiddleware(callback:(req:NextApiRequest, res:NextApiResponse, passedToken)=>{}) {
-    return function(req:NextApiRequest, res:NextApiResponse) {
+    return async function(req:NextApiRequest, res:NextApiResponse) {
+        await runMiddleware(req, res);
         const token = parseToken(req);
         if(!token) {
             res.status(401).send("No estás autorizado");
--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { authMiddleware } from "../../../lib/authMiddleware";
 import { User } from "../../../model/user";
 import { updateUserData } from "../../../controllers/authControllers";
-import {runMiddleware} from "../../../lib/corsMiddleware";
 async function getRequest(req:NextApiRequest, res:NextApiResponse, verifiedToken) {
     const newUser:User = new User(verifiedToken.userId);
     await newUser.pull();
@@ -14,7 +13,6 @@ async function patchRequest(req:NextApiRequest, res:NextApiResponse, verifiedTok
     return updatedUser.data;
 };
 async function handler(req:NextApiRequest, res:NextApiResponse, verifiedToken) {
-    await runMiddleware(req, res);
     if(req.method == "GET") {
         const userData = await getRequest(req, res, verifiedToken);
         res.status(200).json({userData})
