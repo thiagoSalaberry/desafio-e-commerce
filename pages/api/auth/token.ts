ea21953d16505162f3a29d576b2c788d7beb15d7
Thiago <thiagosalaberry99@gmail.com>
2024-01-22 14:21:08 -0300

    auth to [productId]

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -4,15 +4,22 @@ import methods from "micro-method-router";
 import { runMiddleware } from "../../../lib/corsMiddleware";
 import Cors from "cors";
 const cors = Cors({
-    methods: ["GET", "POST", "PATCH"]
-  });
+  methods: ["GET", "POST", "PATCH"],
+});
 //Este endpoint se encarga de recibir un email y un code desde el body
 //De ser correctos, devuelve un token e invalida el código
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    await runMiddleware(req, res);
-    const { email, code } = req.body;
-    if (!email || !code) res.status(400).json({message: "Debes ingresar un email y un código para poder ingresar."});
-    const token = await checkEmailAndCode(email, code);
-    if (!token) res.status(403).json({message: "Los datos ingresados son incorrectos."});
-    res.status(200).json(token);
-};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await runMiddleware(req, res);
+  const { email, code } = req.body;
+  if (!email || !code)
+    res.status(400).json({
+      message: "Debes ingresar un email y un código para poder ingresar.",
+    });
+  const token = await checkEmailAndCode(email, code);
+  if (!token)
+    res.status(403).json({ message: "Los datos ingresados son incorrectos." });
+  res.status(200).json(token);
+}
--- pages/api/products/[productId].ts
+++ pages/api/products/[productId].ts
@@ -1,9 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Product } from "../../../model/product";
+import { authMiddleware } from "../../../lib/authMiddleware";
 //Este endpoint se encarga de recibir un id de un producto y buscarlo en la BdD
-export default async function(req:NextApiRequest, res:NextApiResponse) {
-    const { productId } = req.query;
-    const product = await Product.getProductById(String(productId));
-    if (!product) return res.status(404).json({ message: "El producto que estás buscando no existe" });
-    res.json(product);
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { productId } = req.query;
+  const product = await Product.getProductById(String(productId));
+  if (!product)
+    return res
+      .status(404)
+      .json({ message: "El producto que estás buscando no existe" });
+  res.json(product);
 }
+
+export default authMiddleware(handler);
