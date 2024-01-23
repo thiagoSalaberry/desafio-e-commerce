cc0e57d109ab84814dafede39b9952c6cc85f6d4
Thiago <thiagosalaberry99@gmail.com>
2024-01-22 21:07:04 -0300

    retornamos cors

--- lib/corsMiddleware.ts
+++ lib/corsMiddleware.ts
@@ -3,20 +3,20 @@ import Cors from "cors";
 
 const cors = Cors({
   origin: "*",
-  methods: ["GET", "PATCH", "POST"],
+  methods: ["POST", "GET", "PATCH"],
   preflightContinue: false,
   optionsSuccessStatus: 204,
 });
 
 export function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
-  // return new Promise((resolve, reject) => {
-  //   cors(req, res, (result:any) => {
-  //     if (result instanceof Error) {
-  //       console.log("NO pasó el test de cors")
-  //       return reject(result);
-  //     };
-  //     console.log("Pasó el test de cors")
-  //     return resolve(result);
-  //   })
-  // })
+  return new Promise((resolve, reject) => {
+    cors(req, res, (result: any) => {
+      if (result instanceof Error) {
+        console.log("NO pasó el test de cors");
+        return reject(result);
+      }
+      console.log("Pasó el test de cors");
+      return resolve(result);
+    });
+  });
 }
