c460ee37264a05f9242766f97024684527c4b7ab
Thiago <thiagosalaberry99@gmail.com>
2024-01-22 21:38:19 -0300

    NextCors

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -4,8 +4,7 @@ import { runMiddleware } from "../../../lib/corsMiddleware";
 //Este endpoint se encarga de recibir un email desde el body y crear/encontrar un user. Devuelve el email y un código con fecha de expiración.
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   console.log("arranca el endpoint");
-  const cors = await runMiddleware(req, res);
-  if (!cors) throw Error("No pasó el test de cors");
+  await runMiddleware(req, res);
   console.log("supuestamente pasó el cors middleware");
   const { email } = req.body;
   if (!email)
