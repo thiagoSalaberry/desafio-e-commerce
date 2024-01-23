3ec9351fa712cb6d961816233e1f694c8424da0b
Thiago <thiagosalaberry99@gmail.com>
2024-01-22 21:33:48 -0300

    origin y console.log

--- lib/corsMiddleware.ts
+++ lib/corsMiddleware.ts
@@ -2,13 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
 
 const cors = Cors({
-  origin: "*",
+  origin: "http://localhost:3000",
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS", "HEAD"],
   preflightContinue: false,
   optionsSuccessStatus: 204,
 });
-
 export function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
+  console.log({ cors });
   return new Promise((resolve, reject) => {
     cors(req, res, (result) => {
       if (result instanceof Error) {
