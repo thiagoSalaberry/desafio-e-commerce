bca53a1dafb1beb16b8e1ebffaab102ad626eb56
Thiago <thiagosalaberry99@gmail.com>
2024-01-24 12:19:06 -0300

    cambiamos midlw

--- middleware.ts
+++ middleware.ts
@@ -9,11 +9,19 @@ const allowedOrigins = [
 ];
 export function middleware(req: Request) {
   const url = req.url;
-  if (regex.test(url)) {
+  const origin = req.headers.get("origin");
+  if (
+    /*regex.test(url)*/ url.includes(
+      "https://desafio-e-commerce-five.vercel.app/api"
+    )
+  ) {
     console.log(url);
+    console.log(origin);
+    return NextResponse.next();
   } else {
     console.log("esta es la url que supuestamente no pasa", url);
     console.log("No pasa el test");
+    console.log(origin);
+    throw new Error("No pasa el middleware");
   }
-  return NextResponse.next();
 }
