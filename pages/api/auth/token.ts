796a7917822c020c16ac9d7a247d5499cffc7f43
Thiago <thiagosalaberry99@gmail.com>
2024-01-24 13:34:15 -0300

    a ver el token

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -2,19 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { checkEmailAndCode } from "../../../controllers/authControllers";
 import methods from "micro-method-router";
 import { runMiddleware } from "../../../lib/corsMiddleware";
-import Cors from "cors";
-import { NextResponse } from "next/server";
-const cors = Cors({
-  methods: ["GET", "POST", "PATCH"],
-});
 //Este endpoint se encarga de recibir un email y un code desde el body
 //De ser correctos, devuelve un token e invalida el código
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await runMiddleware(req, res);
   const { email, code } = req.body;
+  console.log(
+    "Estos son el email y el código que le llegan al endpoint",
+    email,
+    code
+  );
+  await runMiddleware(req, res);
   if (!email || !code)
     res.status(400).json({
       message: "Debes ingresar un email y un código para poder ingresar.",
@@ -23,10 +23,10 @@ export default async function handler(
   if (!token)
     res.status(403).json({ message: "Los datos ingresados son incorrectos." });
   res.status(200).json(token);
-  return new NextResponse(null, {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  // return new NextResponse(null, {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Access-Control-Allow-Origin": "*",
+  //   },
+  // });
 }
