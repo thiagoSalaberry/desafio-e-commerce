953720ecc5cdaf29111788edccfb3a3235aa74fa
Thiago <thiagosalaberry99@gmail.com>
2024-01-23 21:46:38 -0300

    a ver ahora url

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,10 @@ const regex = /^http:\/\/localhost:3000(?:\/[^\/]*)*$/;
 const allowedOrigins = ["http://localhost:3000/login"];
 export function middleware(req: Request) {
   const url = req.url;
-  if (regex.test(url)) {
+  if (
+    regex.test(url) ||
+    url == "https://desafio-e-commerce-five.vercel.app/api/auth/token"
+  ) {
     console.log(url);
   } else {
     console.log("esta es la url que supuestamente no pasa", url);
