67314745080feb18f888e6fea517996a651222c1
Thiago <thiagosalaberry99@gmail.com>
2024-02-07 16:56:12 -0300

    cors para la búsqueda de producto

--- pages/api/products/[productId].ts
+++ pages/api/products/[productId].ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Product } from "../../../model/product";
 import { authMiddleware } from "../../../lib/authMiddleware";
+import { runMiddleware } from "../../../lib/corsMiddleware";
 //Este endpoint se encarga de recibir un id de un producto y buscarlo en la BdD
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { productId } = req.query;
+  await runMiddleware(req, res);
   const product = await Product.getProductById(String(productId));
   if (!product)
     return res
