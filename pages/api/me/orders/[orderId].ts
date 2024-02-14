3c866fbbf64e5ff431dff3651aeb4998c2b17234
Thiago <thiagosalaberry99@gmail.com>
2024-02-14 19:21:08 -0300

    order table

--- pages/api/me/orders/[orderId].ts
+++ pages/api/me/orders/[orderId].ts
@@ -1,11 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { authMiddleware } from "../../../../lib/authMiddleware";
 import { getOrder } from "../../../../controllers/orderController";
+import { Order } from "../../../../model/order";
 //Este endpoint se encarga de recibir un token y devolver todas mis órdenes de compra
-async function handler(req:NextApiRequest, res:NextApiResponse, verifiedToken) {
-    const { orderId } = req.query;
-    const order = await getOrder(verifiedToken.userId, String(orderId));
-    res.json({order});
-};
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  verifiedToken
+) {
+  const { orderId } = req.query;
+  // const order = await getOrder(verifiedToken.userId, String(orderId));
+  const order = await Order.getOrderById(String(orderId));
+  await order.pull();
+  res.json({ order });
+}
 
 export default authMiddleware(handler);
